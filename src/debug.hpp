b98ca1ffd3ec01f46f84d2d9b36f2c72059a41cd
torar9 <silber.tomas@outlook.cz>
2021-01-21 15:32:48 +0100

    Add debug macro for Serial.flush()

--- src/debug.hpp
+++ src/debug.hpp
@@ -9,6 +9,7 @@
   #define DBG_PRINT(x) Serial.print(x)
   #define DBG_PRINTDEC(x) Serial.print(x, DEC)
   #define DBG_PRINTLN(x) Serial.println(x)
+  #define DBG_FLUSH() Serial.flush();
 #else
   #define DBG_SERIAL_BEGIN(x)
   #define DBG_SERIAL_SETUP(X)
@@ -16,6 +17,7 @@
   #define DBG_PRINT(x)
   #define DBG_PRINTDEC(x)
   #define DBG_PRINTLN(x)
+  #define DBG_FLUSH()
 #endif
 
 #endif
