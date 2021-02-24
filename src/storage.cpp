ee0109b737b5bb0db780e274aed6cfb44815c670
torar9 <silber.tomas@outlook.cz>
2021-02-24 02:11:40 +0100

    Change json data structure to camel case

--- src/storage.cpp
+++ src/storage.cpp
@@ -66,16 +66,16 @@ void addEventToJSON(DynamicJsonDocument &doc, measurments &event)
     log[F("gasResistance")] = event.gasResistance;
     log[F("temperature")] = event.temperature;
     log[F("batteryLevel")] = event.batteryLevel;
-    log[F("mc_10p0")] = event.spsData.mc_10p0;
-    log[F("mc_1p0")] = event.spsData.mc_1p0;
-    log[F("mc_2p5")] = event.spsData.mc_2p5;
-    log[F("mc_4p0")] = event.spsData.mc_4p0;
-    log[F("nc_0p5")] = event.spsData.nc_0p5;
-    log[F("nc_10p0")] = event.spsData.nc_10p0;
-    log[F("nc_1p0")] = event.spsData.nc_1p0;
-    log[F("nc_2p5")] = event.spsData.nc_2p5;
-    log[F("nc_4p0")] = event.spsData.nc_4p0;
-    log[F("typical_particle_size")] = event.spsData.typical_particle_size;
+    log[F("mc10p0")] = event.spsData.mc_10p0;
+    log[F("mc1p0")] = event.spsData.mc_1p0;
+    log[F("mc2p5")] = event.spsData.mc_2p5;
+    log[F("mc4p0")] = event.spsData.mc_4p0;
+    log[F("nc0p5")] = event.spsData.nc_0p5;
+    log[F("nc10p0")] = event.spsData.nc_10p0;
+    log[F("nc1p0")] = event.spsData.nc_1p0;
+    log[F("nc2p5")] = event.spsData.nc_2p5;
+    log[F("nc4p0")] = event.spsData.nc_4p0;
+    log[F("typicalParticleSize")] = event.spsData.typical_particle_size;
 }
 
 void addEventToJSON(DynamicJsonDocument &doc, statusStruct &status)
